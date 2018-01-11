122112344fadd61b4673cb936df0e57a073d78a2
macfisher <macfisher01@gmail.com>
2018-01-11 18:58:17 +0000

    Filled in conversion calculation

--- examples/ConvertSeconds.java
+++ examples/ConvertSeconds.java
@@ -4,23 +4,17 @@ public class ConvertSeconds
 {
 	public static void main(String[] args)
 	{
-		// 1. Prompt the user for input
-		// 2. Read an integer from the keyboard
-		// 3. Calculate the result
-		// 4. Use printf to display the output
-		// e.g. "5000 seconds = 1 hours, 23 minutes, and 20 seconds"
-		// 5000 seconds = 1.388889
-
 		System.out.print("Enter seconds: ");
 		Scanner reader = new Scanner(System.in);
-		int input_seconds = reader.nextInt();
+		int in_secs = reader.nextInt();
 		reader.close();
-		
-		int hours;
-		int minutes;
-		int seconds;
 
-		System.out.printf("%d\n", hours);
+		int hrs = in_secs / 3600;
+		int mins = (in_secs % 3600) / 60;
+		int secs = (in_secs % 3600) % 60;
+
+		System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds \n",
+			in_secs, hrs, mins, secs);
 		return;
 	}
 }
