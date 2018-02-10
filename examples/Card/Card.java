25844675dee66dbc3ac182c91bbf63559a3f7c65
macfisher <macfisher01@gmail.com>
2018-02-10 16:12:52 +0000

    compareTo and getter/setter methods

--- examples/Card/Card.java
+++ examples/Card/Card.java
@@ -1,20 +1,16 @@
 public class Card
 {
 	public static final String[] SUITS = {
-		"Clubs", "Diamonds", "Hearts", "Spades"};
+		"Clubs", "Diamonds", "Hearts", "Spades"
+	};
 
 	public static final String[] RANKS = {
 		null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
-		"Queen", "King"};
+		"Queen", "King"
+	};
 
 	private int rank;
 	private int suit;
-
-	public Card(int rank, int suit)
-	{
-		this.rank = rank;
-		this.suit = suit;
-	}
 
 	public static void main(String[] args)
 	{
@@ -23,6 +19,13 @@ public class Card
 
 		// test equals overload method
 	}
+
+	public Card(int rank, int suit)
+	{
+		this.rank = rank;
+		this.suit = suit;
+	}
+
 
 	public String toString()
 	{
@@ -33,5 +36,40 @@ public class Card
 	public boolean equals(Card that)
 	{
 		return this.rank == that.rank && this.suit == that.suit;
+	}
+
+	public int compareTo(Card that)
+	{
+		if (this.suit < that.suit)
+		{
+			return -1;
+		}
+
+		if (this.suit > that.suit)
+		{
+			return 1;
+		}
+
+		if (this.rank < that.rank)
+		{
+			return -1;
+		}
+
+		if (this.rank > that.rank)
+		{
+			return 1;
+		}
+
+		return 0;
+	}
+
+	public int getRank()
+	{
+		return this.rank;
+	}
+
+	public int getSuit()
+	{
+		return this.suit;
 	}
 }
