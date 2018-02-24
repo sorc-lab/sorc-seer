66887ef991c549b5de38505afc06b8301e338c22
macfisher <macfisher01@gmail.com>
2018-02-24 17:07:16 +0000

    Deck class and tinkering w/ Java packages

---
+++ examples/Cards/Deck.java
@@ -0,0 +1,37 @@
+import Card;
+
+
+public class Deck
+{
+	private Card[] cards;
+
+	public static void main(String[] args)
+	{
+		Deck deck = new Deck();
+		print();
+	}
+
+	public Deck(int n)
+	{
+		this.cards = new Card[52];
+		int index = 0;
+
+		for (int suit = 0; suit <= 3; suit++)
+		{
+			for (int rank = 1; rank <= 13; rank++)
+			{
+				this.cards[index] = new Card(rank, suit);
+				index++;
+			}
+		}
+	}
+
+	public void print()
+	{
+		for (int i = 0; i < this.cards.length; i++)
+		{
+			System.out.println(this.cards[i]);
+		}
+	}
+}
+
