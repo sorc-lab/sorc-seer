2524ad4cc160c6cd92a6efb1808bbac06dc45da3
macfisher <macfisher01@gmail.com>
2018-03-08 15:49:08 +0000

    Func. duration in milliseconds

--- examples/Cards/Test.java
+++ examples/Cards/Test.java
@@ -4,14 +4,24 @@ public class Test
 
 	public static void main(String[] args) {
 		System.out.println("Executing swapCards algorithm...");
+		
+		long startTime = System.currentTimeMillis();
+		System.out.println("startTime = " + startTime);
+
 		deck.swapCards();
-		System.out.println("swapCards DONE.");
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("endTime = " + endTime);
+
+		long duration = (endTime - startTime);
+		System.out.println("duration = " + duration + " milliseconds" + "\n");
 
 		System.out.println("Checking for card duplicates...");
 		if (deck.checkDuplicateCards()) {
 			System.out.println("ERROR: System found duplicate cards.");
 		}
-
-		deck.print();
+		
+		//System.out.println();
+		//deck.print();
 	}
 }
