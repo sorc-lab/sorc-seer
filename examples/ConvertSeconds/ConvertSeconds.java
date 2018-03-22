50f25fea9bf8baa39fccfe402797c52ada3b8a59
macfisher <macfisher01@gmail.com>
2018-03-22 17:08:17 +0000

    Made ConvertSeconds testable

--- examples/ConvertSeconds/ConvertSeconds.java
+++ examples/ConvertSeconds/ConvertSeconds.java
@@ -4,17 +4,23 @@ public class ConvertSeconds
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Enter seconds: ");
-		Scanner reader = new Scanner(System.in);
-		int in_secs = reader.nextInt();
-		reader.close();
+		//System.out.print("Enter seconds: ");
+		//Scanner reader = new Scanner(System.in);
+		//double in_secs = reader.nextDouble();
+		//reader.close();
 
-		int hrs = in_secs / 3600;
-		int mins = (in_secs % 3600) / 60;
-		int secs = (in_secs % 3600) % 60;
+		double in_secs = Double.parseDouble(args[0]);
 
-		System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds \n",
-			in_secs, hrs, mins, secs);
+		double hrs = in_secs / 3600;
+		double mins = (in_secs % 3600) / 60;
+		double secs = (in_secs % 3600) % 60;
+
+		System.out.printf(
+			"%.2f seconds = %.2f hours, %.2f minutes, and %.2f seconds \n",
+			in_secs, hrs, mins, secs
+		);
+
+
 		return;
 	}
 }
