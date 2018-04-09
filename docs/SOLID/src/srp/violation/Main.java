9701bc2915e5681150dfb437ed6fa18d7d2caf58
macfisher <macfisher01@gmail.com>
2018-04-09 16:05:50 -0600

    Netlist methods

--- docs/SOLID/src/srp/violation/Main.java
+++ docs/SOLID/src/srp/violation/Main.java
@@ -5,6 +5,14 @@ public class Main
 	public static void main(String[] args)
 	{
 		Netlist netlist = new Netlist();
-		netlist.getIp("SORC-T530");
+		String id = netlist.getIdFromIp("10.10.43.144");
+		//String ip = netlist.getIpFromId("SORC-T530");
+	
+		System.out.println(id);
+		//System.out.println(ip);
+		
+		// replace w/ getIpFromId()
+		boolean status = netlist.getStatus("10.10.43.140");
+		System.out.println(status);
 	}
 }
--- docs/SOLID/src/srp/violation/Netlist.java
+++ docs/SOLID/src/srp/violation/Netlist.java
@@ -2,15 +2,16 @@ package srp.violation;
 
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Netlist {
-	// reportNetworkStatus: print network status
-	// getIp: return ip
-	// getStatus: return ONLINE or OFFLINE
-	// getId: return id
+	/** TODO: pintReport() method */
+	/** TODO: Add exceptions and fix getIpFromId and getIdFromIp */
+	/** TODO: .gitignore to filter out Eclipse IDE metadata */
 	
 	HashMap<String, String> network = new HashMap<String, String>();
-	String ip;
 	
 	public Netlist()
 	{
@@ -19,26 +20,54 @@ public class Netlist {
 		this.network.put("10.10.43.142", "SORC-STAGE");
 		this.network.put("10.10.43.143", "SORC-WORK01");
 		this.network.put("10.10.43.144", "SORC-T530");
-		
-		for (Entry<String, String> entry : network.entrySet()) {
-            if (entry.getValue().equals("SORC-T530")) {
-                System.out.println(entry.getKey());
-            }
-        }
 	}
 	
-	public String getIp(String id)
+	public String getIpFromId(String id)
 	{
+		String ip = "";
+		
 		for (Entry<String, String> entry : network.entrySet()) {
             if (entry.getValue().equals(id)) {
-                this.ip = entry.getKey();
+            	ip = entry.getKey();
+            } else {
+            	ip = "Unable to get IP address from that ID.";
             }
         }
 		
-		return this.ip;
+		return ip;
 	}
 	
+	public String getIdFromIp(String ip) { return network.get(ip); }
 	
-	
-	
+	public boolean getStatus(String ip)
+	{
+		String[] splitIp = ip.split("\\.");
+		
+		byte nib1 = (byte) Integer.parseInt(splitIp[0]);
+		byte nib2 = (byte) Integer.parseInt(splitIp[1]);
+		byte nib3 = (byte) Integer.parseInt(splitIp[2]);
+		byte nib4 = (byte) Integer.parseInt(splitIp[3]);
+		
+		InetAddress inetAddr;
+		
+		try {
+			inetAddr = InetAddress.getByAddress(
+				new byte[] { nib1, nib2, nib3, nib4 }
+			);
+			
+			try {
+				if (inetAddr.isReachable(200)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (IOException e) {
+				
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
