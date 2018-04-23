db8cffb2cdb6389c33b59c497a18f1e36cd0a699
macfisher <macfisher01@gmail.com>
2018-04-23 04:22:45 +0000

    Builder example links

--- docs/Patterns/src/builder/Main.java
+++ docs/Patterns/src/builder/Main.java
@@ -4,6 +4,9 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Builder initial commit.");
+		// https://www.javaworld.com/article/2074938/core-java/too-many-parameters-in-java-methods-part-3-builder-pattern.html
+		// https://dzone.com/articles/design-patterns-the-builder-pattern
+		// https://jlordiales.me/2012/12/13/the-builder-pattern-in-practice/
+		// http://www.vogella.com/tutorials/DesignPatternBuilder/article.html
 	}
 }
