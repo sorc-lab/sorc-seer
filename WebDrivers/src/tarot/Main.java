d55a1664fd49df71eedd7cd503001a7bff456953
macfisher <macfisher01@gmail.com>
2018-04-24 09:51:06 -0600

    SetupWebDriver class and cleaned out Main

--- WebDrivers/src/tarot/Main.java
+++ WebDrivers/src/tarot/Main.java
@@ -1,45 +1,20 @@
 package tarot;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Main
-{
-	static WebDriver driver;
-	
+{	
 	public static void main(String[] args) throws Exception
 	{
-		// test CLI execution
-		String baseDir = System.getProperty("user.dir");
-		String phantomjs = baseDir + "/WebDriverDependencies/phantomjs.exe";
+		SetupWebDriver setup = new SetupWebDriver();
+		WebDriver driver = setup.getWebDriver();
 		
-		// set phantomjs.exe file path via DesiredCapabilities
-		DesiredCapabilities capability = new DesiredCapabilities();
-		capability.setCapability(
-			PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-			phantomjs
-		);	
-		driver = new PhantomJSDriver(capability);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
-		tmp();
-	}
-	
-	public static void tmp() throws IOException
-	{
-		driver.get("https://www.google.com");
+		driver.get("http://www.google.com");
 		
 		// get title via JS executor
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		String pageTitle = (String) js.executeScript("return document.title");
-		System.out.println("Page title: " + pageTitle);
-		
-		// get some elements to test out...
+		System.out.println("Page title: " + pageTitle); 
 	}
 }
---
+++ WebDrivers/src/tarot/SetupWebDriver.java
@@ -0,0 +1,31 @@
+package tarot;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SetupWebDriver
+{
+	private WebDriver _driver;
+	
+	public WebDriver getWebDriver()
+	{
+		String baseDir = System.getProperty("user.dir");
+		String phantomjs = baseDir + "/WebDriverDependencies/phantomjs.exe";
+		
+		// set phantomjs.exe file path via DesiredCapabilities
+		DesiredCapabilities capability = new DesiredCapabilities();
+		capability.setCapability(
+			PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+			phantomjs
+		);	
+		_driver = new PhantomJSDriver(capability);
+		_driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		return _driver;
+	}
+
+}
