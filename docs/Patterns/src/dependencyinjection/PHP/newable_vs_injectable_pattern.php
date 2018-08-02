2c18e0652f27062892236691ee0980f89b65eed2
macfisher <macfisher01@gmail.com>
2018-08-02 09:10:25 -0600

    Example for 'newable vs. injectable pattern'

---
+++ docs/Patterns/src/dependencyinjection/PHP/newable_vs_injectable_pattern.php
@@ -0,0 +1,117 @@
+<?php
+
+// ensure that only a single instance exists for each class.
+class Singleton
+{
+    // implements the 'singleton' design pattern.
+    static function &getInstance ($class, $arg1 = null) {
+        static $instances = array();  // array of instance names
+
+        if (array_key_exists($class, $instances)) {
+            // instance exists in array, so use it
+            $instance =& $instances[$class];         
+        }
+        else {
+            // load the class file (if not already loaded)
+            if (!class_exists($class)) {
+                switch ($class) {
+                    case 'date_class':
+                      require_once 'std.datevalidation.class.inc';
+                      break;
+                    case 'encryption_class':
+                      require_once 'std.encryption.class.inc';
+                      break;
+                    case 'validation_class':
+                        require_once 'std.validation.class.inc';
+                        break;
+                    default:
+                        require_once "classes/$class.class.inc";
+                        break;
+                } // switch
+            } // if
+
+            // instance does not exist, so create it
+            $instances[$class] = new $class($arg1);
+            $instance =& $instances[$class];
+        } // if
+
+       return $instance;
+    } // getInstance        
+} // singleton
+
+
+// Entity class, newable
+class Author
+{
+    private $firstName;
+    private $lastName;
+ 
+    public function __construct($fistName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+ 
+    public function getFirstName() { return $this->firstName; }
+    public function getLastName() { return $this->lastName; }
+
+    // obj. should have a getData() array, pulls from DB record on Author table
+    // getFirst/LastName should pull from $data array
+    // validate $id before even instantiating a DB connection obj.
+}
+
+
+// Entity class, newable
+class Question
+{
+    private $author;
+    private $question;
+
+    public function __construct($question) { $this->question = $question; }
+ 
+    public function getAuthor($firstName, $lastName) { // should only need pass $id
+        $this->author = Singleton::getInstance('Author', $firstName, $lastName);
+        return $this->author;
+    }
+
+    public function getQuestion() { return $this->question; }
+}
+
+
+
+/*
+class Author {
+    private $firstName;
+    private $lastName;
+ 
+    public function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+ 
+    public function getFirstName() {
+        return $this->firstName;
+    }
+ 
+    public function getLastName() {
+        return $this->lastName;
+    }
+}
+
+
+class Question {
+    private $author;
+    private $question;
+    public function __construct($question, $authorFirstName, $authorLastName) {
+        $this->author = new Author($authorFirstName, $authorLastName);
+        $this->question = $question;
+    }
+ 
+    public function getAuthor() {
+        return $this->author;
+    }
+ 
+    public function getQuestion() {
+        return $this->question;
+    }
+}
+*/
